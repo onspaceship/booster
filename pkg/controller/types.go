8e0d957e9082fdfacead5cfe1378d71043be125d
Tim Dorr <git@timdorr.com>
2021-08-05 17:11:21 -0400

    Create a kpack Build

---
+++ pkg/controller/types.go
@@ -0,0 +1,7 @@
+package controller
+
+const (
+	AppIdAnnotation   = "onspaceship.com/app-id"
+	BuildIdAnnotation = "onspaceship.com/build-id"
+	GitRefAnnotation  = "onspaceship.com/git-ref"
+)
--- pkg/socket/handler/handle_build.go
+++ pkg/socket/handler/handle_build.go
@@ -1,9 +1,94 @@
 package handler
 
-import "github.com/apex/log"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
 
-func handleBuild(payload interface{}) {
+	"github.com/onspaceship/booster/pkg/client"
+	"github.com/onspaceship/booster/pkg/config"
+	"github.com/onspaceship/booster/pkg/controller"
+
+	"github.com/apex/log"
+
+	kpackapi "github.com/pivotal/kpack/pkg/apis/build/v1alpha1"
+	kpackclient "github.com/pivotal/kpack/pkg/client/clientset/versioned/typed/build/v1alpha1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+type buildPayload struct {
+	AppId      string `json:"app_id"`
+	BuildId    string `json:"build_id"`
+	TeamHandle string `json:"team_handle"`
+	AppHandle  string `json:"app_handle"`
+	GitRef     string `json:"git_ref"`
+	GitSHA     string `json:"git_sha"`
+}
+
+func handleBuild(jsonPayload []byte, options *config.SocketOptions) {
+	var payload buildPayload
+	err := json.Unmarshal(jsonPayload, &payload)
+	if err != nil {
+		log.WithError(err).Info("Payload is invalid")
+		return
+	}
+
+	githubCreds, err := client.NewClient().CoreAppGithubCredentials(payload.AppId)
+	if err != nil {
+		log.WithError(err).Info("Couldn't fetch GitHub credentials")
+		return
+	}
+
 	log.WithField("payload", payload).Info("Handling build")
+
+	name := fmt.Sprintf("%s-%s-%s", payload.TeamHandle, payload.AppHandle, payload.BuildId)
+	tag := fmt.Sprintf("gcr.io/onspaceship/%s/%s", payload.TeamHandle, payload.AppHandle)
+	gitURL := fmt.Sprintf("https://x-access-token:%s@github.com/%s.git", githubCreds.Token, githubCreds.RepoPath)
+
+	client := kpackclient.NewForConfigOrDie(ctrl.GetConfigOrDie())
+
+	_, err = client.Builds(options.Namespace).Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil && !apierrors.IsNotFound(err) {
+		log.WithError(err).Info("Couldn't fetch Build from Kubernetes")
+		return
+	}
+
+	if err == nil {
+		log.WithError(err).WithField("app-id", payload.AppId).WithField("build-id", payload.BuildId).Info("Build already exists")
+		return
+	}
+
+	build := &kpackapi.Build{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			Annotations: map[string]string{
+				controller.AppIdAnnotation:   payload.AppId,
+				controller.BuildIdAnnotation: payload.BuildId,
+				controller.GitRefAnnotation:  payload.GitRef,
+			},
+		},
+		Spec: kpackapi.BuildSpec{
+			Tags: []string{tag},
+			Source: kpackapi.SourceConfig{
+				Git: &kpackapi.Git{
+					URL:      gitURL,
+					Revision: payload.GitSHA,
+				},
+			},
+			Builder: kpackapi.BuildBuilderSpec{
+				Image: "paketobuildpacks/builder:full",
+			},
+			ServiceAccount: "kpack",
+		},
+	}
+
+	_, err = client.Builds(options.Namespace).Create(context.Background(), build, metav1.CreateOptions{})
+	if err == nil {
+		log.WithError(err).WithField("app-id", payload.AppId).WithField("build-id", payload.BuildId).Info("Error creating Build")
+		return
+	}
 }
 
 func init() {
